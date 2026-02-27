c047b7aad770406345d81a404cd9935672272196
FelipeOG1 <feliog1234@gmail.com>
2026-02-26 18:12:14 -0600

    render first image

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include <SDL3/SDL.h>
-#include <iostream>
-
+#include <SDL3_image/SDL_image.h>
 
 struct SDLState {
     SDL_Window* window;
@@ -9,6 +8,7 @@ struct SDLState {
 };
 
 void cleanup(SDLState* state);
+
 
 int main(int argc, char* argv[]) {
     SDLState state;
@@ -21,7 +21,7 @@ int main(int argc, char* argv[]) {
     int w = 800;
     int h = 600;
 
-    state.window = SDL_CreateWindow("demo", w, h, 0);
+    state.window = SDL_CreateWindow("demo", w, h, SDL_WINDOW_RESIZABLE);
     
     if (!state.window){
         SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "ERROR", "Error creating window", nullptr);
@@ -40,8 +40,21 @@ int main(int argc, char* argv[]) {
         return 1;
         
     }
+    int logW = 640;
+    int logH = 320;
 
+    SDL_SetRenderLogicalPresentation(state.renderer, logW, logH, SDL_LOGICAL_PRESENTATION_LETTERBOX);
 
+    SDL_FRect dst {
+        .x = 0,
+        .y = 10,
+        .w = 32,
+        .h = 32
+    };
+
+    SDL_Texture* idltex = IMG_LoadTexture(state.renderer, "data/player-stand-1.png");
+    SDL_SetTextureScaleMode(idltex, SDL_SCALEMODE_NEAREST);
+    
     
     bool running = true;
     while (running) {
@@ -58,17 +71,23 @@ int main(int argc, char* argv[]) {
             
             }           
         }
+
+
         
-        SDL_SetRenderDrawColor(state.renderer, 255, 255, 255, 255);
+        // set backbuffer white 
+        SDL_SetRenderDrawColor(state.renderer, 20, 10, 30, 255);
         SDL_RenderClear(state.renderer);
-
-
+        SDL_RenderTexture(state.renderer, idltex, nullptr, &dst);
+        
+        // move back buffer to front 
         SDL_RenderPresent(state.renderer);
         
         
     }
-    
+    SDL_DestroyTexture(idltex);
     cleanup(&state);   
+
+
     return 0;
 
 }
